e5fd26ed0688e43b1f5d62a77157d483df062d85
muzammilsayyed2606 <116951699+muzammilsayyed2606@users.noreply.github.com>
2023-09-26 03:12:57 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,209 @@
+provider "aws" {
+  region  = local.region
+  profile = "non-prod"
+
+  # Make it faster by skipping something
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
+
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  platform    = "intuitive"
+  vpc_name    = "${local.platform}-vpc"
+  region      = "eu-west-1"
+  bucket_name = "${local.platform}-s3-bucket-for-interview"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  user_data = <<-EOT
+   #!/bin/bash
+   echo "Hello from Muzzammil !!"
+   EOT
+
+  ec2_instances = {
+    first = {
+      instance_type     = "t3.micro"
+      availability_zone = element(module.vpc.azs, 0)
+      subnet_id         = element(module.vpc.private_subnets, 0)
+      root_block_device = [
+        {
+          encrypted   = true
+          volume_type = "gp3"
+          throughput  = 200
+          volume_size = 50
+          tags = {
+            Name = "first-instance-volume"
+          }
+        }
+      ]
+    }
+    second = {
+      instance_type     = "t3.small"
+      availability_zone = element(module.vpc.azs, 1)
+      subnet_id         = element(module.vpc.private_subnets, 1)
+      root_block_device = [
+        {
+          encrypted   = true
+          volume_type = "gp2"
+          volume_size = 50
+          tags = {
+            Name = "second-instance-volume"
+          }
+        }
+      ]
+    }
+  }
+
+  tags = {
+    Example = local.vpc_name
+  }
+}
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.vpc_name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+
+  private_subnet_names = ["Private Subnet One", "Private Subnet Two"]
+  # public_subnet_names omitted to show default vpc_name generation for all three subnets
+
+  create_database_subnet_group  = false
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_vpn_gateway = true
+
+  enable_dhcp_options              = true
+  dhcp_options_domain_name         = "service.consul"
+  dhcp_options_domain_name_servers = ["127.0.0.1", "10.10.0.2"]
+
+  # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
+  enable_flow_log                      = false
+  create_flow_log_cloudwatch_log_group = false
+  create_flow_log_cloudwatch_iam_role  = false
+
+  tags = local.tags
+}
+
+################################################################################
+# S3 Bucket
+################################################################################
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = local.bucket_name
+
+  force_destroy       = true
+  acceleration_status = "Suspended"
+  request_payer       = "BucketOwner"
+
+  tags = {
+    Owner = "Muzzammil"
+  }
+
+  # Bucket policies
+  attach_policy = false
+
+  # S3 Bucket Ownership Controls
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_ownership_controls
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerEnforced"
+
+  versioning = {
+    status     = true
+    mfa_delete = false
+  }
+
+  lifecycle_rule = [
+    {
+      id      = "cleanup-after-7-days"
+      enabled = true
+
+      filter = {
+        tags = {
+          objects = "eligible-to-delete"
+        }
+      }
+
+      transition = [
+        {
+          days          = 30
+          storage_class = "ONEZONE_IA"
+          }, {
+          days          = 60
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days                         = 90
+        expired_object_delete_marker = true
+      }
+
+      noncurrent_version_expiration = {
+        newer_noncurrent_versions = 5
+        days                      = 30
+      }
+    },
+  ]
+}
+
+################################################################################
+# EC2 instances
+################################################################################
+
+module "ec2_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = "ec2-security-group"
+  description = "Security group for EC2 instance"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["ssh-tcp"]
+  egress_rules        = ["all-all"]
+
+  tags = local.tags
+}
+
+module "ec2_instances" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  for_each = local.ec2_instances
+
+  name = "${local.platform}-ec2-${each.key}"
+
+  instance_type          = each.value.instance_type
+  availability_zone      = each.value.availability_zone
+  subnet_id              = each.value.subnet_id
+  vpc_security_group_ids = [module.ec2_security_group.security_group_id]
+
+  enable_volume_tags = false
+  root_block_device  = lookup(each.value, "root_block_device", [])
+
+  tags = local.tags
+}
