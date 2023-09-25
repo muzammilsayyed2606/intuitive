9a53c8c6ff5f71acf511c770065f19428e61cf39
muzammilsayyed2606 <116951699+muzammilsayyed2606@users.noreply.github.com>
2023-09-26 04:32:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,14 @@
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
+provider "aws" {
   region  = local.region
+  shared_config_files=["/home/muzammilsayyed/.aws/config"]
+  shared_credentials_files = ["/home/muzammilsayyed/.aws/credentials"]
 
   # Make it faster by skipping something
   skip_metadata_api_check     = true
   skip_region_validation      = true
   skip_credentials_validation = true
   skip_requesting_account_id  = true
+}
 
 data "aws_availability_zones" "available" {}
 
